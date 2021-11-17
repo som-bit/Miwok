4dda6f4c93a756402f62c83b451382303f02c569
som-bit <sr8180525@gmail.com>
2021-11-17 21:23:30 +0530

    TabLayout

--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -17,7 +17,7 @@ public class WordAdapter extends ArrayAdapter<Word> {
 
     private int mColorResourceId;
 
-    //    AudioManager mAudioManager;
+
     public WordAdapter(Activity context, ArrayList<Word> words,int colorResourceId) {
         // Here, we initialize the ArrayAdapter's internal storage for the context and the list.
         // the second argument is used when the ArrayAdapter is populating a single TextView.
